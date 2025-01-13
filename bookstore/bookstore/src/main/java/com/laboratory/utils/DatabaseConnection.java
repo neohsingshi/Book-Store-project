631c480db3765ae91da8ccbff944d7e84167d8fc
BBBILLY-122 <bingye2004@gmail.com>
2025-01-13 20:37:06 +0800

    Update DatabaseConnection.java

--- bookstore/bookstore/src/main/java/com/laboratory/utils/DatabaseConnection.java
+++ bookstore/bookstore/src/main/java/com/laboratory/utils/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/bookstore"; // 数据库连接
-    private static final String USER = "root"; // 用户名
-    private static final String PASSWORD = "mmq183193"; // 密码
+    private static final String URL = "jdbc:mysql://localhost:3306/bookstore"; // Database connection
+    private static final String USER = "root"; // Username
+    private static final String PASSWORD = "mmq183193"; // password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);

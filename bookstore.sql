79c953a7105f4fe42fb6ade19c1b391a7706e217
wyh1201 <496839294@qq.com>
2025-01-17 22:54:57 +0800

    Update bookstore.sql

--- bookstore.sql
+++ bookstore.sql
@@ -22,32 +22,32 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `books`;
 CREATE TABLE `books`  (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '书籍ID',
-  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '书名',
-  `price` decimal(10, 2) NOT NULL COMMENT '价格',
-  `stock` int NOT NULL DEFAULT 0 COMMENT '库存数量',
-  `category` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '类别',
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'bookID',
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'book title',
+  `price` decimal(10, 2) NOT NULL COMMENT 'price',
+  `stock` int NOT NULL DEFAULT 0 COMMENT 'stockpile',
+  `category` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'form',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '书籍信息表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = 'Book Information Sheet' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of books
 -- ----------------------------
-INSERT INTO `books` VALUES (2, '123', 123.00, 114, '小说');
-INSERT INTO `books` VALUES (3, '1232', 123.00, 110, '科幻');
+INSERT INTO `books` VALUES (2, '123', 123.00, 114, 'fiction');
+INSERT INTO `books` VALUES (3, '1232', 123.00, 110, 'sci-fi');
 
 -- ----------------------------
 -- Table structure for orders
 -- ----------------------------
 DROP TABLE IF EXISTS `orders`;
 CREATE TABLE `orders`  (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '订单ID，自增主键',
-  `book_id` int NOT NULL COMMENT '书籍ID，外键关联到books表中的id字段',
-  `quantity` int NOT NULL COMMENT '购买数量，表示用户购买该书籍的数量',
-  `order_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '订单日期，默认值为当前时间戳，记录订单创建的时间',
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'Order ID, self-incrementing primary key',
+  `book_id` int NOT NULL COMMENT 'Book ID, foreign key associated to id field in books table',
+  `quantity` int NOT NULL COMMENT 'Quantity Purchased, which indicates the number of copies of the book purchased by the user',
+  `order_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Order Date, defaults to the current timestamp, records when the order was created',
   `user_identifier` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '订单表，用于记录用户购买书籍的信息' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = 'Order form, used to record information about the books purchased by users' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of orders
@@ -63,23 +63,23 @@ INSERT INTO `orders` VALUES (5, 3, 2, '2024-12-30 13:51:45', '123');
 -- ----------------------------
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users`  (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '用户ID',
-  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '姓名',
-  `gender` enum('男','女') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '性别',
-  `age` int NULL DEFAULT NULL COMMENT '年龄',
-  `identity` enum('用户','管理员') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '身份（用户、管理员）',
-  `identifier` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'ID号（学号或教工号或身份证号等）',
-  `phone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '联系电话',
-  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '邮箱',
-  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码',
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'userID',
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'name',
+  `gender` enum('male','female') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'gender',
+  `age` int NULL DEFAULT NULL COMMENT 'age',
+  `identity` enum('user','manager') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'identity（user、manager）',
+  `identifier` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'IDnumber（Student number or faculty number or ID number, etc.）',
+  `phone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'phonenumber',
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'email',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'password',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `identifier`(`identifier` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 19 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户信息表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 19 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = 'user information sheet' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
-INSERT INTO `users` VALUES (17, 'mmq', '男', NULL, '用户', '123', '15888888888', '123@qq.com', '1234');
-INSERT INTO `users` VALUES (18, 'mmq1', '男', NULL, '管理员', '1234', '15888888888', '123@qq.com', '123');
+INSERT INTO `users` VALUES (17, 'mmq', 'male', NULL, 'user', '123', '15888888888', '123@qq.com', '1234');
+INSERT INTO `users` VALUES (18, 'mmq1', 'male', NULL, 'manager', '1234', '15888888888', '123@qq.com', '123');
 
 SET FOREIGN_KEY_CHECKS = 1;
